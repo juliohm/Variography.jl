9457b0f342ddc5e64debda844f61cf2e677cddb1
Júlio Hoffimann <julio.hoffimann@gmail.com>
2018-04-02 21:33:20 -0700

    Migrate implementation from various GeoStats.jl packages

--- src/Variography.jl
+++ src/Variography.jl
@@ -1,6 +1,48 @@
+# ------------------------------------------------------------------
+# Copyright (c) 2018, Júlio Hoffimann Mendes <juliohm@stanford.edu>
+# Licensed under the ISC License. See LICENCE in the project root.
+# ------------------------------------------------------------------
+
 __precompile__()
+
 module Variography
 
-# Package code goes here.
+importall GeoStatsBase
+
+using Distances
+using SpecialFunctions: besselk
+using RecipesBase
+
+# won't be neeeded in Julia v0.7
+using Parameters
+
+# extend result_type and pairwise for theoretical variograms
+import Distances: result_type, pairwise
+
+# variogram models
+include("empirical_variograms.jl")
+include("theoretical_variograms.jl")
+
+# plot recipes
+include("plotrecipes/empirical_variograms.jl")
+include("plotrecipes/theoretical_variograms.jl")
+
+export
+  # empirical variograms
+  EmpiricalVariogram,
+
+  # theoretical variograms
+  AbstractVariogram,
+  GaussianVariogram,
+  ExponentialVariogram,
+  MaternVariogram,
+  SphericalVariogram,
+  CubicVariogram,
+  PentasphericalVariogram,
+  PowerVariogram,
+  SineHoleVariogram,
+  CompositeVariogram,
+  isstationary,
+  pairwise
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,33 @@
 using Variography
+using GeoStatsDevTools
+using GeoStatsImages
+using Plots; gr(size=(600,400))
 using Base.Test
+using VisualRegressionTests
+
+# setup GR backend for Travis CI
+ENV["GKSwstype"] = "100"
+ENV["PLOTS_TEST"] = "true"
+
+# list of maintainers
+maintainers = ["juliohm"]
+
+# environment settings
+istravis = "TRAVIS" ∈ keys(ENV)
+ismaintainer = "USER" ∈ keys(ENV) && ENV["USER"] ∈ maintainers
+datadir = joinpath(@__DIR__,"data")
+
+fname2D = joinpath(datadir,"data2D.tsv")
+data2D = readtable(fname2D, delim='\t', coordnames=[:x,:y])
+
+# list of tests
+testfiles = [
+  "empirical_variograms.jl",
+  "theoretical_variograms.jl"
+]
 
 @testset "Variography.jl" begin
-    # Write your own tests here.
-    @test 1 == 2
+  for testfile in testfiles
+    include(testfile)
+  end
 end
