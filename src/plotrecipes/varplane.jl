c81d69342fd007e2310b77415b325bd75be7739c
Júlio Hoffimann <julio.hoffimann@gmail.com>
2018-12-12 23:55:13 -0200

    Update varplane plot recipe

--- src/plotrecipes/varplane.jl
+++ src/plotrecipes/varplane.jl
@@ -5,9 +5,10 @@
 
 @userplot VarPlane
 
-@recipe function f(vp::VarPlane; direction=nothing,
+@recipe function f(vp::VarPlane; normal=nothing,
                    nlags=20, maxlag=nothing,
-                   nangs=50, atol=10., btol=0.95)
+                   nangs=50, atol=10., btol=0.95,
+                   showrange=true, varmodel=GaussianVariogram)
   # get inputs
   spatialdata = vp.args[1]
   var₁ = vp.args[2]
@@ -17,6 +18,9 @@
   rs = Vector{Float64}(undef, nlags)
   θs = range(0, stop=π, length=nangs)
   zs = Matrix{Float64}(undef, nlags, nangs)
+
+  # ranges for each angle
+  ls = Vector{Float64}(undef, nangs)
 
   # loop over half plane
   for (j, θ) in Iterators.enumerate(θs)
@@ -30,17 +34,38 @@
       isnan(ys[i]) && (ys[i] = ys[i-1])
     end
 
+    # save variogram for this angle
     zs[:,j] = ys
+
+    if showrange
+      γtheo = fit(varmodel, γ)
+      ls[j] = range(γtheo)
+    end
   end
 
   # exploit symmetry
   θs = range(0, stop=2π, length=2nangs)
   zs = hcat(zs, zs)
 
-  seriestype --> :heatmap
   projection --> :polar
-  color --> :curl
   ylim --> (0, Inf)
 
-  θs, rs, zs
+  # plot variogram plane
+  @series begin
+    seriestype --> :heatmap
+    color --> :curl
+
+    θs, rs, zs
+  end
+
+  # plot ranges
+  if showrange
+    @series begin
+      seriestype --> :path
+      linecolor --> :black
+      label --> "range"
+
+      θs, ls
+    end
+  end
 end
