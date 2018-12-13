adabe487e371b2a2c4274ced0e444f04fe3996a2
Júlio Hoffimann <julio.hoffimann@gmail.com>
2018-12-13 10:52:39 -0200

    Add test for varplane plot recipe

--- test/plotrecipes.jl
+++ test/plotrecipes.jl
@@ -8,5 +8,18 @@
       refimg = joinpath(datadir,"HScatter.png")
       @test test_images(VisualTest(plot_hscatter, refimg), popup=!istravis, tol=0.15) |> success
     end
+
+    # UNCOMMENT WHEN GR ADDS SUPPORT TO POLAR PLOTS
+    # @testset "varplane" begin
+      # imgdata = readdlm(joinpath(datadir,"anisotropic.tsv"))
+      # geodata = RegularGridData{Float64}(Dict(:z => imgdata))
+
+      # function plot_varplane(fname)
+        # varplane(geodata, :z, maxlag=50., size=(500,500))
+        # png(fname)
+      # end
+      # refimg = joinpath(datadir,"VarPlane.png")
+      # @test test_images(VisualTest(plot_varplane, refimg), popup=!istravis) |> success
+    # end
   end
 end
