4b12dc94ae9b3703ef80e43d8120818f1117a746
Júlio Hoffimann <julio.hoffimann@gmail.com>
2019-01-02 17:40:23 -0200

    Move @inbounds statements up in pairwise.jl

--- src/pairwise.jl
+++ src/pairwise.jl
@@ -24,15 +24,15 @@ Non-allocating pairwise evaluation.
 """
 function pairwise!(Γ, γ::Variogram, X::AbstractMatrix)
   m, n = size(X)
-  for j=1:n
+  @inbounds for j=1:n
     xj = view(X, :, j)
     for i=j+1:n
       xi = view(X, :, i)
-      @inbounds Γ[i,j] = γ(xi, xj)
+      Γ[i,j] = γ(xi, xj)
     end
-    @inbounds Γ[j,j] = γ(xj, xj)
+    Γ[j,j] = γ(xj, xj)
     for i=1:j-1
-      @inbounds Γ[i,j] = Γ[j,i] # leverage the symmetry
+      Γ[i,j] = Γ[j,i] # leverage the symmetry
     end
   end
 end
@@ -49,15 +49,15 @@ function pairwise(γ::Variogram, domain::AbstractDomain{T,N},
   n = length(locations)
   R = result_type(γ, xi, xj)
   Γ = Matrix{R}(undef, n, n)
-  for j=1:n
+  @inbounds for j=1:n
     coordinates!(xj, domain, locations[j])
     for i=j+1:n
       coordinates!(xi, domain, locations[i])
-      @inbounds Γ[i,j] = γ(xi, xj)
+      Γ[i,j] = γ(xi, xj)
     end
-    @inbounds Γ[j,j] = γ(xj, xj)
+    Γ[j,j] = γ(xj, xj)
     for i=1:j-1
-      @inbounds Γ[i,j] = Γ[j,i] # leverage the symmetry
+      Γ[i,j] = Γ[j,i] # leverage the symmetry
     end
   end
 
@@ -78,11 +78,11 @@ function pairwise(γ::Variogram, domain::AbstractDomain{T,N},
   n = length(locations₂)
   R = result_type(γ, xi, xj)
   Γ = Array{R}(undef, m, n)
-  for j=1:n
+  @inbounds for j=1:n
     coordinates!(xj, domain, locations₂[j])
     for i=1:m
       coordinates!(xi, domain, locations₁[i])
-      @inbounds Γ[i,j] = γ(xi, xj)
+      Γ[i,j] = γ(xi, xj)
     end
   end
 
