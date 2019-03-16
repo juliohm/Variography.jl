ad6b1bf024a25efe9c247bbe909292e9286fb3e5
Júlio Hoffimann <julio.hoffimann@gmail.com>
2019-03-16 13:29:14 -0300

    Add nlags option to theoretical variogram plot recipe

--- src/plotrecipes/theoretical_variograms.jl
+++ src/plotrecipes/theoretical_variograms.jl
@@ -2,9 +2,9 @@
 # Licensed under the ISC License. See LICENCE in the project root.
 # ------------------------------------------------------------------
 
-@recipe function f(γ::Variogram; maxlag=3.)
+@recipe function f(γ::Variogram; nlags=100, maxlag=3.)
   # start at 1e-6 instead of 0 to avoid nugget artifact in plot
-  h = range(1e-6, stop=maxlag, length=100)
+  h = range(1e-6, stop=maxlag, length=nlags)
 
   seriestype --> :path
   xlim --> (0, maxlag)
