f19168a1a77838add38e18d3aa4219734df779c2
Júlio Hoffimann <julio.hoffimann@gmail.com>
2020-06-16 23:22:27 -0300

    Improve plot recipe for theoretical variogram

--- src/plotrecipes/theoretical.jl
+++ src/plotrecipes/theoretical.jl
@@ -2,7 +2,10 @@
 # Licensed under the ISC License. See LICENSE in the project root.
 # ------------------------------------------------------------------
 
-@recipe function f(γ::Variogram; nlags=100, maxlag=3.)
+_hmax(γ::Variogram) = 3range(γ)
+_hmax(γ::PowerVariogram) = 3.
+
+@recipe function f(γ::Variogram; nlags=100, maxlag=_hmax(γ))
   # start at 1e-6 instead of 0 to avoid nugget artifact in plot
   h = range(1e-6, stop=maxlag, length=nlags)
 
