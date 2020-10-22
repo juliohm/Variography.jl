7617981a8dcd7d64e0ed2f4fccf1f3cc773556ff
Júlio Hoffimann <julio.hoffimann@gmail.com>
2020-10-22 18:31:24 -0300

    Format code in nested.jl

--- src/nested.jl
+++ src/nested.jl
@@ -27,19 +27,19 @@ end
 NestedVariogram(cs, γs) = NestedVariogram{length(cs)}(cs, γs)
 
 # variogram interface
-(g::NestedVariogram)(h) = raw(sum(c*γ(h) for (c, γ) in zip(g.cs, g.γs)))
-(g::NestedVariogram)(x, y) = raw(sum(c*γ(x, y) for (c, γ) in zip(g.cs, g.γs)))
-sill(g::NestedVariogram) = raw(sum(c*sill(γ) for (c, γ) in zip(g.cs, g.γs)))
-nugget(g::NestedVariogram) = raw(sum(c*nugget(γ) for (c, γ) in zip(g.cs, g.γs)))
-Base.range(g::NestedVariogram) = maximum(range(γ) for γ in g.γs)
+(g::NestedVariogram)(h)          = raw(sum(c*γ(h) for (c, γ) in zip(g.cs, g.γs)))
+(g::NestedVariogram)(x, y)       = raw(sum(c*γ(x, y) for (c, γ) in zip(g.cs, g.γs)))
+sill(g::NestedVariogram)         = raw(sum(c*sill(γ) for (c, γ) in zip(g.cs, g.γs)))
+nugget(g::NestedVariogram)       = raw(sum(c*nugget(γ) for (c, γ) in zip(g.cs, g.γs)))
+Base.range(g::NestedVariogram)   = maximum(range(γ) for γ in g.γs)
 isstationary(g::NestedVariogram) = all(isstationary(γ) for γ in g.γs)
 
 # algebraic structure
-*(c, γ::Variogram) = NestedVariogram((c,), (γ,))
-*(c, γ::NestedVariogram{N}) where {N} = NestedVariogram(ntuple(i->c.*γ.cs[i], N), γ.γs)
-+(γ₁::Variogram, γ₂::Variogram) = NestedVariogram((I, I), (γ₁, γ₂))
-+(γ₁::NestedVariogram, γ₂::Variogram) = NestedVariogram((γ₁.cs..., I), (γ₁.γs..., γ₂))
-+(γ₁::Variogram, γ₂::NestedVariogram) = NestedVariogram((I, γ₂.cs...), (γ₁, γ₂.γs...))
+*(c, γ::Variogram)                          = NestedVariogram((c,), (γ,))
+*(c, γ::NestedVariogram)                    = NestedVariogram(map(x->c.*x, γ.cs), γ.γs)
++(γ₁::Variogram, γ₂::Variogram)             = NestedVariogram((I, I), (γ₁, γ₂))
++(γ₁::NestedVariogram, γ₂::Variogram)       = NestedVariogram((γ₁.cs..., I), (γ₁.γs..., γ₂))
++(γ₁::Variogram, γ₂::NestedVariogram)       = NestedVariogram((I, γ₂.cs...), (γ₁, γ₂.γs...))
 +(γ₁::NestedVariogram, γ₂::NestedVariogram) = NestedVariogram((γ₁.cs..., γ₂.cs...), (γ₁.γs..., γ₂.γs...))
 
 # ------------
@@ -50,11 +50,11 @@ function Base.show(io::IO, ::NestedVariogram{N}) where N
 end
 
 function Base.show(io::IO, ::MIME"text/plain", g::NestedVariogram)
-  println(io, g)
-  println(io, "  structures")
-  coeffs = [Float64.(c) for c in raw.(g.cs)]
+  coeffs = raw.(g.cs)
   models = [nameof(typeof(γ)) for γ in g.γs]
   params = ["range=$(range(γ)), sill=$(sill(γ)), nugget=$(nugget(γ))" for γ in g.γs]
+  println(io, g)
+  println(io, "  structures")
   lines  = ["    └─$γ($p)" for (γ, p) in zip(models, params)]
   println(io, join(lines, "\n"))
   println(io, "  coefficients")
