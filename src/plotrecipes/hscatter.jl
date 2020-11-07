c0e481bd93936d420a0a987cf920a5f49f177d98
Júlio Hoffimann <julio.hoffimann@gmail.com>
2020-11-07 20:15:46 -0300

    Update hscatter

--- src/plotrecipes/hscatter.jl
+++ src/plotrecipes/hscatter.jl
@@ -4,7 +4,7 @@
 
 @userplot HScatter
 
-@recipe function f(hs::HScatter; lags=nothing, tol=1e-1, distance=Euclidean())
+@recipe function f(hs::HScatter; lag=0, tol=1e-1, distance=Euclidean())
   # get inputs
   sdata = hs.args[1]
   var₁ = hs.args[2]
@@ -23,52 +23,43 @@
   pairs = [(i,j) for j in 1:n for i in j:m]
   ds = [evaluate(distance, view(X₁,:,i), view(X₂,:,j)) for (i,j) in pairs]
 
-  # use quartiles by default
-  lags == nothing && (lags = quantile(ds, [.00,.25,.50,.75]))
-
   xguide --> var₁
   yguide --> var₂
   legend --> false
   aspect_ratio --> :equal
-  layout --> (1, length(lags))
 
-  for (i, lag) in enumerate(lags)
-    # find indices with given lag
-    match = findall(abs.(ds .- lag) .< tol)
+  # find indices with given lag
+  match = findall(abs.(ds .- lag) .< tol)
 
-    if isempty(match)
-      @warn "no points were found with lag = $lag, skipping..."
-      continue
-    end
+  if isempty(match)
+    @warn "no points were found with lag = $lag, skipping..."
+    return nothing
+  end
 
-    # scatter plot coordinates
-    mpairs = view(pairs, match)
-    x = z₁[first.(mpairs)]
-    y = z₂[last.(mpairs)]
+  # scatter plot coordinates
+  mpairs = view(pairs, match)
+  x = z₁[first.(mpairs)]
+  y = z₂[last.(mpairs)]
 
-    # plot identity line
-    @series begin
-      subplot := i
-      seriestype := :path
-      seriescolor := :black
-      primary := false
-      linestyle := :dash
+  # plot identity line
+  @series begin
+    seriestype := :path
+    seriescolor := :black
+    primary := false
+    linestyle := :dash
 
-      xmin, xmax = extrema(x)
-      ymin, ymax = extrema(y)
-      vmin = min(xmin, ymin)
-      vmax = max(xmax, ymax)
+    xmin, xmax = extrema(x)
+    ymin, ymax = extrema(y)
+    vmin = min(xmin, ymin)
+    vmax = max(xmax, ymax)
 
-      [vmin, vmax], [vmin, vmax]
-    end
+    [vmin, vmax], [vmin, vmax]
+  end
 
-    # plot h-scatter
-    @series begin
-      subplot := i
-      seriestype := :scatter
-      title --> @sprintf "lag = %.1f" lag
+  # plot h-scatter
+  @series begin
+    seriestype := :scatter
 
-      x, y
-    end
+    x, y
   end
 end
--- test/plotrecipes.jl
+++ test/plotrecipes.jl
@@ -3,7 +3,11 @@
     @testset "h-scatter" begin
       @plottest begin
         sdata = readgeotable(joinpath(datadir,"samples2D.tsv"), coordnames=(:x,:y))
-        hscatter(sdata, :value, lags=[0.,1.,2.,3.], layout=(2,2), size=(600,600))
+        p0 = hscatter(sdata, :value, lag=0)
+        p1 = hscatter(sdata, :value, lag=1)
+        p2 = hscatter(sdata, :value, lag=2)
+        p3 = hscatter(sdata, :value, lag=3)
+        plot(p0, p1, p2, p3, layout=(2,2), size=(600,600))
       end joinpath(datadir,"hscatter.png") !istravis
     end
   end
