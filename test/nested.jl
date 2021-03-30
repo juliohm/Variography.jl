86b3e95cc97cbe9ac21f584709c83f8220bdbe97
hameye <meyerhadrien96@gmail.com>
2021-03-30 19:59:47 +0200

    Change nested formula causing memory allocations,add corresponding tests

--- test/nested.jl
+++ test/nested.jl
@@ -4,14 +4,20 @@
   @test nugget(γ) ≈ 0.3
   @test sill(γ) ≈ 1.0
   @test range(γ) ≈ 50.0
+  @test (@elapsed sill(γ)) < 100e-6
+  @test (@allocated sill(γ)) < 32
   γ = 2.0*NuggetEffect(0.2)
   @test nugget(γ) ≈ 0.4
   @test sill(γ) ≈ 0.4
   @test range(γ) ≈ 0.0
+  @test (@elapsed sill(γ)) < 100e-6
+  @test (@allocated sill(γ)) < 32
 
   # sill is defined for nested models
   γ = GaussianVariogram(sill=1.) + ExponentialVariogram(sill=2.)
   @test sill(γ) == 3.0
+  @test (@elapsed sill(γ)) < 100e-6
+  @test (@allocated sill(γ)) < 32
 
   # nugget is defined for nested models
   γ₁ = GaussianVariogram()
@@ -23,6 +29,8 @@
   @test isstationary(γ)
   @test sill(γ) == 3.0
   @test !isstationary(γ + PowerVariogram())
+  @test (@elapsed sill(γ)) < 100e-6
+  @test (@allocated sill(γ)) < 32
 
   # result type is defined for nested models
   # see https://github.com/JuliaEarth/GeoStats.jl/issues/121 
@@ -50,16 +58,22 @@
   @test γ(Point(10.,0.), Point(0.,0.)) ≈ sill(γ)
   @test isstationary(γ)
 
-  # test constructor explicitly
+  # test coe-6tructor explicitly
   γ = NestedVariogram((1., 2.), (ExponentialVariogram(), SphericalVariogram()))
   @test sill(γ) == 3.0
   @test range(γ) == 1.0
   @test nugget(γ) == 0.0
+  @test (@elapsed sill(γ)) < 100e-6
+  @test (@allocated sill(γ)) < 32
 
   # test individual structures
   γ = SphericalVariogram() + 2ExponentialVariogram() + NuggetEffect(10.0)
   @test structures(γ) == (10.0, (1.0, 2.0), (SphericalVariogram(), ExponentialVariogram()))
+  @test (@elapsed sill(γ)) < 100e-6
+  @test (@allocated sill(γ)) < 32
   γ = SphericalVariogram(sill=2.0) + ExponentialVariogram(nugget=0.1)
   @test structures(γ) == (0.1, (2.0, 0.9), (SphericalVariogram(), ExponentialVariogram()))
   @test structures(SphericalVariogram()) == (0.0, (1.0,), (SphericalVariogram(),))
+  @test (@elapsed sill(γ)) < 100e-6
+  @test (@allocated sill(γ)) < 32
 end
