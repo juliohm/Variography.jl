32e204e17b523fd754cc631517bb6793d422e0ff
hameye <meyerhadrien96@gmail.com>
2021-03-31 09:45:04 +0200

    Update nugget formula and add corresponding memory allocation tests

--- test/nested.jl
+++ test/nested.jl
@@ -12,12 +12,14 @@
   @test range(γ) ≈ 0.0
   @test (@elapsed sill(γ)) < 1e-6
   @test (@allocated sill(γ)) < 32
+  @test (@allocated nugget(γ)) < 32
 
   # sill is defined for nested models
   γ = GaussianVariogram(sill=1.) + ExponentialVariogram(sill=2.)
   @test sill(γ) == 3.0
   @test (@elapsed sill(γ)) < 1e-6
   @test (@allocated sill(γ)) < 32
+  @test (@allocated nugget(γ)) < 32
 
   # nugget is defined for nested models
   γ₁ = GaussianVariogram()
@@ -31,6 +33,7 @@
   @test !isstationary(γ + PowerVariogram())
   @test (@elapsed sill(γ)) < 1e-6
   @test (@allocated sill(γ)) < 32
+  @test (@allocated nugget(γ)) < 32
 
   # result type is defined for nested models
   # see https://github.com/JuliaEarth/GeoStats.jl/issues/121 
@@ -65,15 +68,18 @@
   @test nugget(γ) == 0.0
   @test (@elapsed sill(γ)) < 1e-6
   @test (@allocated sill(γ)) < 32
+  @test (@allocated nugget(γ)) < 32
 
   # test individual structures
   γ = SphericalVariogram() + 2ExponentialVariogram() + NuggetEffect(10.0)
   @test structures(γ) == (10.0, (1.0, 2.0), (SphericalVariogram(), ExponentialVariogram()))
   @test (@elapsed sill(γ)) < 1e-6
   @test (@allocated sill(γ)) < 32
+  @test (@allocated nugget(γ)) < 32
   γ = SphericalVariogram(sill=2.0) + ExponentialVariogram(nugget=0.1)
   @test structures(γ) == (0.1, (2.0, 0.9), (SphericalVariogram(), ExponentialVariogram()))
   @test structures(SphericalVariogram()) == (0.0, (1.0,), (SphericalVariogram(),))
   @test (@elapsed sill(γ)) < 1e-6
   @test (@allocated sill(γ)) < 32
+  @test (@allocated nugget(γ)) < 32
 end
