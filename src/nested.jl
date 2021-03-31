9d78689bf58910169accc241ce75acb9abf27e24
Hadrien Meyer <57682091+hameye@users.noreply.github.com>
2021-03-31 15:30:25 +0200

    Change nested formula causing memory allocations,add corresponding tests (#45)
    * Change nested formula causing memory allocations,add corresponding tests
    
    * Decrease threshold for test and correct typo
    
    * Update nugget formula and add corresponding memory allocation tests
    
    * add elapsed time for nugget
    
    * Fix typo in test for nested variograms

--- src/nested.jl
+++ src/nested.jl
@@ -27,10 +27,10 @@ end
 NestedVariogram(cs, γs) = NestedVariogram{typeof(cs),typeof(γs)}(cs, γs)
 
 # variogram interface
-(g::NestedVariogram)(h)                  = raw(sum(c*γ(h) for (c, γ) in zip(g.cs, g.γs)))
-(g::NestedVariogram)(x::Point, y::Point) = raw(sum(c*γ(x, y) for (c, γ) in zip(g.cs, g.γs)))
-sill(g::NestedVariogram)                 = raw(sum(c*sill(γ) for (c, γ) in zip(g.cs, g.γs)))
-nugget(g::NestedVariogram)               = raw(sum(c*nugget(γ) for (c, γ) in zip(g.cs, g.γs)))
+(g::NestedVariogram)(h)                  = raw(sum(g.cs .* map(γ -> γ(h), g.γs)))
+(g::NestedVariogram)(x::Point, y::Point) = raw(sum(g.cs .* map(γ -> γ(x, y), g.γs)))
+sill(g::NestedVariogram)                 = raw(sum(g.cs .* map(sill, g.γs)))
+nugget(g::NestedVariogram)               = raw(sum(g.cs .* map(nugget, g.γs)))
 Base.range(g::NestedVariogram)           = maximum(range(γ) for γ in g.γs)
 isstationary(g::NestedVariogram)         = all(isstationary(γ) for γ in g.γs)
 
