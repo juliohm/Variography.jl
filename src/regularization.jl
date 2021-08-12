38203a6f982e74d5213459caae72d62e970eae6e
Júlio Hoffimann <julio.hoffimann@gmail.com>
2021-08-12 09:21:01 -0300

    Refactor regularization.jl

--- src/regularization.jl
+++ src/regularization.jl
@@ -3,19 +3,22 @@
 # ------------------------------------------------------------------
 
 function (γ::Variogram)(U::Geometry, v::Point)
-  α = _reg_spacing(U)
-  us = sample(U, MinDistanceSampling(α))
+  us = _reg_sample(U)
   mean(γ(u, v) for u in us)
 end
 
 (γ::Variogram)(u::Point, V::Geometry) = γ(V, u)
 
 function (γ::Variogram)(U::Geometry, V::Geometry)
-  αᵤ = _reg_spacing(U)
-  αᵥ = _reg_spacing(V)
-  us = sample(U, MinDistanceSampling(αᵤ))
-  vs = sample(V, MinDistanceSampling(αᵥ))
+  us = _reg_sample(U)
+  vs = _reg_sample(V)
   mean(γ(u, v) for u in us, v in vs)
+end
+
+# helper function to sample points within a geometry
+function _reg_sample(g::Geometry)
+  α = _reg_spacing(g)
+  sample(g, MinDistanceSampling(α))
 end
 
 # helper function to compute the spacing of points within
@@ -23,13 +26,13 @@ end
 function _reg_spacing(g::Geometry)
   μ = measure(g)
   d = paramdim(g)
-  n = _maxpoints(g)
+  n = _reg_maxpoints(g)
   (μ / n) ^ (1 / d)
 end
 
 # recommended maximum number of points inside a geometry
 # Journel, A. & Huijbregts, Ch.J. Mining Geostatistics page 97
-function _maxpoints(g::Geometry)
+function _reg_maxpoints(g::Geometry)
   paramdim(g) == 1 && return 10
   paramdim(g) == 2 && return 6*6
   paramdim(g) == 3 && return 4*4*4
