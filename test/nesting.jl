fd5aaa8097e20ebcb98d3daf6927adcc08a08f1a
Júlio Hoffimann <julio.hoffimann@gmail.com>
2021-10-06 09:53:34 -0300

    Relax timing tests in test/nesting.jl

--- test/nesting.jl
+++ test/nesting.jl
@@ -12,16 +12,16 @@
   @test nugget(γ) ≈ 0.4
   @test sill(γ) ≈ 0.4
   @test range(γ) ≈ 0.0
-  @test (@elapsed sill(γ)) < 1e-6
-  @test (@elapsed nugget(γ)) < 1e-6
+  @test (@elapsed sill(γ)) < 1e-5
+  @test (@elapsed nugget(γ)) < 1e-5
   @test (@allocated sill(γ)) < 32
   @test (@allocated nugget(γ)) < 32
 
   # sill is defined for nested models
   γ = GaussianVariogram(sill=1.) + ExponentialVariogram(sill=2.)
   @test sill(γ) == 3.0
-  @test (@elapsed sill(γ)) < 1e-6
-  @test (@elapsed nugget(γ)) < 1e-6
+  @test (@elapsed sill(γ)) < 1e-5
+  @test (@elapsed nugget(γ)) < 1e-5
   @test (@allocated sill(γ)) < 32
   @test (@allocated nugget(γ)) < 32
 
@@ -35,8 +35,8 @@
   @test isstationary(γ)
   @test sill(γ) == 3.0
   @test !isstationary(γ + PowerVariogram())
-  @test (@elapsed sill(γ)) < 1e-6
-  @test (@elapsed nugget(γ)) < 1e-6
+  @test (@elapsed sill(γ)) < 1e-5
+  @test (@elapsed nugget(γ)) < 1e-5
   @test (@allocated sill(γ)) < 32
   @test (@allocated nugget(γ)) < 32
 
@@ -71,23 +71,23 @@
   @test sill(γ) == 3.0
   @test range(γ) == 1.0
   @test nugget(γ) == 0.0
-  @test (@elapsed sill(γ)) < 1e-6
-  @test (@elapsed nugget(γ)) < 1e-6
+  @test (@elapsed sill(γ)) < 1e-5
+  @test (@elapsed nugget(γ)) < 1e-5
   @test (@allocated sill(γ)) < 32
   @test (@allocated nugget(γ)) < 32
 
   # test individual structures
   γ = SphericalVariogram() + 2ExponentialVariogram() + NuggetEffect(10.0)
   @test structures(γ) == (10.0, (1.0, 2.0), (SphericalVariogram(), ExponentialVariogram()))
-  @test (@elapsed sill(γ)) < 1e-6
-  @test (@elapsed nugget(γ)) < 1e-6
+  @test (@elapsed sill(γ)) < 1e-5
+  @test (@elapsed nugget(γ)) < 1e-5
   @test (@allocated sill(γ)) < 32
   @test (@allocated nugget(γ)) < 32
   γ = SphericalVariogram(sill=2.0) + ExponentialVariogram(nugget=0.1)
   @test structures(γ) == (0.1, (2.0, 0.9), (SphericalVariogram(), ExponentialVariogram()))
   @test structures(SphericalVariogram()) == (0.0, (1.0,), (SphericalVariogram(),))
-  @test (@elapsed sill(γ)) < 1e-6
-  @test (@elapsed nugget(γ)) < 1e-6
+  @test (@elapsed sill(γ)) < 1e-5
+  @test (@elapsed nugget(γ)) < 1e-5
   @test (@allocated sill(γ)) < 32
   @test (@allocated nugget(γ)) < 32
 end
