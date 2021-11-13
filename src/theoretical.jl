a1ade2c0609842202001f3e2d4daf23d5d2b6803
Júlio Hoffimann <julio.hoffimann@gmail.com>
2021-11-13 14:25:44 -0300

    Refactor theoretical.jl

--- src/theoretical.jl
+++ src/theoretical.jl
@@ -104,12 +104,12 @@ Optionally, use a custom distance `d`.
   nugget::T = 0.0
   distance::D = Euclidean()
 end
-(γ::GaussianVariogram)(h) = begin
+function (γ::GaussianVariogram)(h)
   # add small eps to nugget
   # for numerical stability
   s = γ.sill
   r = γ.range
-  n = γ.nugget + convert(typeof(s), 1e-6)
+  n = γ.nugget + typeof(s)(1e-6)
   (s - n) * (1 - exp(-3(h/r)^2)) + (h > 0) * n
 end
 isstationary(::Type{<:GaussianVariogram}) = true
@@ -126,7 +126,12 @@ Optionally, use a custom distance `d`.
   nugget::T = 0.0
   distance::D = Euclidean()
 end
-(γ::ExponentialVariogram)(h) = (γ.sill - γ.nugget) * (1 - exp(-3(h/γ.range))) + (h > 0) * γ.nugget
+function (γ::ExponentialVariogram)(h)
+  s = γ.sill
+  r = γ.range
+  n = γ.nugget
+  (s - n) * (1 - exp(-3(h/r))) + (h > 0) * n
+end
 isstationary(::Type{<:ExponentialVariogram}) = true
 
 """
@@ -142,7 +147,7 @@ A Matérn variogram with sill `s`, range `r` and nugget `n`. The parameter
   order::T  = 1.0
   distance::D = Euclidean()
 end
-(γ::MaternVariogram)(h) = begin
+function (γ::MaternVariogram)(h::T) where {T}
   s = γ.sill
   r = γ.range
   n = γ.nugget
@@ -150,10 +155,11 @@ end
 
   # shift lag by machine precision to
   # avoid explosion at the origin
-  h2 = h + eps(typeof(h))
-  h3 = sqrt(2.0ν)h2/r
+  h′ = sqrt(2ν)*(h + eps(T)) / r
+  Β  = besselk(ν, h′)
+  Γ  = gamma(ν)
 
-  (s - n) * (1 - 2.0^(1 - ν)/gamma(ν) * h3^ν * besselk(ν, h3)) + (h > 0) * n
+  (s - n) * (1 - 2^(1 - ν) / Γ * h′^ν * Β) + (h′ > 0) * n
 end
 isstationary(::Type{<:MaternVariogram}) = true
 
@@ -169,12 +175,20 @@ Optionally, use a custom distance `d`.
   nugget::T = 0.0
   distance::D = Euclidean()
 end
-(γ::SphericalVariogram)(h) = begin
+function (γ::SphericalVariogram)(h::T) where {T}
   s = γ.sill
   r = γ.range
   n = γ.nugget
 
-  (h < r) * (s - n) * (1.5(h/r) - 0.5(h/r)^3) + (h ≥ r) * (s - n) + (h > 0) * n
+  # constants
+  c1 = T(3) / T(2)
+  c2 = T(1) / T(2)
+  s1 = c1*(h/r) - c2*(h/r)^3
+  s2 = T(1)
+
+  (h < r) * (s - n) * s1 +
+  (h ≥ r) * (s - n) * s2 +
+  (h > 0) * n
 end
 isstationary(::Type{<:SphericalVariogram}) = true
 
@@ -190,13 +204,21 @@ Optionally, use a custom distance `d`.
   nugget::T = 0.0
   distance::D = Euclidean()
 end
-(γ::CubicVariogram)(h) = begin
+function (γ::CubicVariogram)(h::T) where {T}
   s = γ.sill
   r = γ.range
   n = γ.nugget
 
-  (h < r) * (s - n) * (7*(h/r)^2 - (35/4)*(h/r)^3 + (7/2)*(h/r)^5 - (3/4)*(h/r)^7) +
-  (h ≥ r) * (s - n) + (h > 0) * n
+  # constants
+  c1 = T(35) / T(4)
+  c2 = T(7)  / T(2)
+  c3 = T(3)  / T(4)
+  s1 = 7*(h/r)^2 - c1*(h/r)^3 + c2*(h/r)^5 - c3*(h/r)^7
+  s2 = T(1)
+
+  (h < r) * (s - n) * s1 +
+  (h ≥ r) * (s - n) * s2 +
+  (h > 0) * n
 end
 isstationary(::Type{<:CubicVariogram}) = true
 
@@ -212,13 +234,21 @@ Optionally, use a custom distance `d`.
   nugget::T = 0.0
   distance::D = Euclidean()
 end
-(γ::PentasphericalVariogram)(h) = begin
+function (γ::PentasphericalVariogram)(h::T) where {T}
   s = γ.sill
   r = γ.range
   n = γ.nugget
 
-  (h < r) * (s - n) * ((15/8)*(h/r) - (5/4)*(h/r)^3 + (3/8)*(h/r)^5) +
-  (h ≥ r) * (s - n) + (h > 0) * n
+  # constants
+  c1 = T(15) / T(8)
+  c2 = T(5)  / T(4)
+  c3 = T(3)  / T(8)
+  s1 = c1*(h/r) - c2*(h/r)^3 + c3*(h/r)^5
+  s2 = T(1)
+
+  (h < r) * (s - n) * s1 +
+  (h ≥ r) * (s - n) * s2 +
+  (h > 0) * n
 end
 isstationary(::Type{<:PentasphericalVariogram}) = true
 
@@ -234,7 +264,12 @@ Optionally, use a custom distance `d`.
   exponent::T = 1.0
   distance::D = Euclidean()
 end
-(γ::PowerVariogram)(h) = γ.scaling*h^γ.exponent + (h > 0) * γ.nugget
+function (γ::PowerVariogram)(h)
+  s = γ.scaling
+  a = γ.exponent
+  n = γ.nugget
+  s * h^a + (h > 0) * n
+end
 isstationary(::Type{<:PowerVariogram}) = false
 
 """
@@ -249,16 +284,17 @@ Optionally, use a custom distance `d`.
   nugget::T = 0.0
   distance::D = Euclidean()
 end
-(γ::SineHoleVariogram)(h) = begin
+function (γ::SineHoleVariogram)(h::T) where {T}
   s = γ.sill
   r = γ.range
   n = γ.nugget
 
   # shift lag by machine precision to
   # avoid explosion at the origin
-  h = h + eps(eltype(h))
+  h′ = h + eps(T)
+  c  = T(π)
 
-  (s - n) * (1 - sin(π*h/r)/(π*h/r)) + (h > 0) * n
+  (s - n) * (1 - sin(c*h′/r)/(c*h′/r)) + (h′ > 0) * n
 end
 isstationary(::Type{<:SineHoleVariogram}) = true
 
@@ -274,7 +310,9 @@ Optionally use a custom distance `d`.
   distance::D = Euclidean()
 end
 NuggetEffect(n) = NuggetEffect(nugget=n)
-(γ::NuggetEffect)(h) = (h > 0) * γ.nugget
-Base.range(::NuggetEffect{T,D}) where {T,D} = zero(T)
+function (γ::NuggetEffect)(h)
+  (h > 0) * γ.nugget
+end
+Base.range(::NuggetEffect{T}) where {T} = zero(T)
 sill(γ::NuggetEffect) = γ.nugget
 isstationary(::Type{<:NuggetEffect}) = true
