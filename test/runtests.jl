d11a588fad2de7fe768d9c48015f821faf525626
Júlio Hoffimann <julio.hoffimann@gmail.com>
2023-01-25 16:21:33 -0300

    Bump version

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,18 +6,10 @@ using Unitful
 using Distances
 using LinearAlgebra
 using CSV, DelimitedFiles
-using Plots; gr(size=(600,400))
-using ReferenceTests, ImageIO
 using Test, Random
 import CoDa: Composition
 
-# workaround for GR warnings
-ENV["GKSwstype"] = "100"
-
 # environment settings
-isCI = "CI" ∈ keys(ENV)
-islinux = Sys.islinux()
-visualtests = !isCI || (isCI && islinux)
 datadir = joinpath(@__DIR__,"data")
 
 # list of tests
